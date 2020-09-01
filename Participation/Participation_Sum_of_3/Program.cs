4ef3635e3532caec6f3a5be02cfe9294902f433b
Grace Dennis <G.Dennis@ou.edu>
2020-09-01 17:57:08 -0500

    saved  participation GitHub

--- Participation/Participation_Sum_of_3/Program.cs
+++ Participation/Participation_Sum_of_3/Program.cs
@@ -6,7 +6,7 @@ namespace Participation_Sum_of_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
