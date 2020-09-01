cffbb1b70b22c2923af019615a651cd57b240d04
Grace Dennis <G.Dennis@ou.edu>
2020-09-01 12:14:20 -0500

    finished input & outut

--- InClassExample/InputOutput/Program.cs
+++ InClassExample/InputOutput/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿/* 
+ * Collab with class
+*/
+
+using System;
 
 namespace InputOutput
 {
     class Program
     {
-        static void Main(string[] args)
+        const string CLASS_NAME = "MIS 3013";
+
+       static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string firstName, lastName;
+            string firstName = "Grace";
+            string lastName = "Dennis";
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your GPA? >>");
+            string gpaInput = Console.ReadLine();
+            double gpa = Convert.ToDouble(gpaInput);
+
+            Console.WriteLine(firstName + " " + lastName + " is teaching " + CLASS_NAME + " with a GPA of " + gpa.ToString("N2")); 
         }
     }
 }
