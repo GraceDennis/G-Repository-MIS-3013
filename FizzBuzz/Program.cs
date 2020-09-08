b547b3815bc6674d804fc93dbd4b53d4ca9cdf40
Grace Dennis <G.Dennis@ou.edu>
2020-09-08 11:59:23 -0500

    started Fizz Buzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+            if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+       
+            Console.WriteLine(number);
+        }
+    }
+}
