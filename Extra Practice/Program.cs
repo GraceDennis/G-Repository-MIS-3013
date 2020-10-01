a90a3622360f59285721f94af817b2c896d387c6
Grace Dennis <G.Dennis@ou.edu>
2020-10-01 11:54:55 -0500

    get help on File path

---
+++ Extra Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch05Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\grace\Downloads\84-0 (1).txt";
+
+            string[] linesOfFile = File.ReadAllLines(filePath);
+            int i = 0;
+            foreach (var line in linesOfFile)
+            {
+                string[] pieces = line.Split(' ');
+                if (i % 15 == 0 && i != 0)
+                {
+                    Console.ReadKey();
+                }
+            
+            }
+            
+        }
+
+    }
+
+}
