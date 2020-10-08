5c28f436aef1132f0fec6d85fb078398d61c44b2
Grace Dennis <G.Dennis@ou.edu>
2020-10-08 10:02:08 -0500

    almost finished min/avg/max

---
+++ (P)Collections_Min_Max_Avg/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _P_Collections_Min_Max_Avg
+{
+    class Program
+    {
+
+        //Ask the user to enter all of their exam grades. 
+        //Once they are done, calculate the minimum score, the maximum 
+        //score and the average score for all of their scores.  
+        // Perform this using at least 2 Loops (can be the same type of 
+        //loop) and not any built in functions.
+        // EXTRA: Calculate the Mode of the exam scores***
+        static void Main(string[] args)
+        {
+            List<double> examgrades = new List<double>();
+            string answer;
+            do
+            {
+                Console.WriteLine("Please enter your first exam grade. >>");
+                double examgrade = Convert.ToDouble(Console.ReadLine());
+
+                examgrades.Add(examgrade);
+
+                Console.WriteLine("Please enter your second exam grade. >>");
+                answer = Console.ReadLine();
+                
+            } 
+            while (answer.ToLower() == "yes");
+
+           
+            double max;
+            double average;
+            double min;
+
+            foreach (var examgrade in examgrades)
+            {
+                if (examgrade < examgrades[0])
+                    if (examgrade <= min)
+                    {
+                        min = examgrade;
+
+                        Console.WriteLine($"Your minimum grade is {min}.");
+                    }
+                do
+                {
+                    if (examgrade > examgrades[0])
+                    {
+                        max = examgrade;
+                        Console.WriteLine($"Your maximum grade is {max}.");
+                    }
+                    else if (examgrade == examgrades[0])
+                    {
+                        average = examgrade;
+                        Console.WriteLine($"Your average grade is {average}.");
+                    }
+
+                } while (true);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Book_Practice_Cont/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Practice_Cont
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is ch 4 example 5
+            double balance, interestRate, targetBalance;
+            Console.WriteLine("what is ur current balance?");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is ur current annual interest rate?");
+            interestRate = 1 + Convert.ToDouble(Console.ReadLine()) / 100.00;
+            Console.WriteLine("what balance would you like to have?");
+            //part 1
+            targetBalance = Convert.ToDouble(Console.ReadLine());
+
+            //part 2
+            int totalYears = 0;
+            while (balance < targetBalance)
+            {
+                balance *= interestRate;
+                ++totalYears;
+            }
+            Console.WriteLine($"In {totalYears} year{(totalYears == 1 ? "" : "s")} " +
+                $"you'll have a balance of {balance}.");
+
+            if (totalYears == 0)
+            {
+                Console.WriteLine("tbh, u did not need a calculator");
+            } Console.ReadKey();
+
+        }
+    }
+}
+
