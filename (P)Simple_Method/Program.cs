db7252b5e454faace1d230097937ca1c8bf0a973
Grace Dennis <G.Dennis@ou.edu>
2020-10-20 12:57:08 -0500

    finished Simple Method

---
+++ (P)Simple_Method/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P_Simple_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Animal = AnimalType();
+            string response = Speak(Animal);
+            Console.WriteLine(response); 
+
+             Console.ReadKey();
+        }
+
+        static string AnimalType()
+        {
+            Console.WriteLine(" << Enter an animal type >>");
+            string AnimalType = Console.ReadLine();
+
+            return AnimalType;
+        }
+
+        static string Speak(string AnimalType)
+            {
+            string response = AnimalType;
+
+                if (response == "monkey")
+                {
+                    Console.WriteLine("OO OO ah ah");
+                }
+                else if (response == "goat")
+                {
+
+                    Console.WriteLine("hee haw bahhhh");
+                }
+                else 
+                {
+                    Console.WriteLine("Bark woof");
+                }
+
+            return response;
+            Console.ReadKey();
+                
+            }
+
+    }
+}
--- Practice_Loops/Program.cs
+++ Practice_Loops/Program.cs
@@ -6,16 +6,42 @@ namespace Practice_Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter an integer:");
+            /*  Console.WriteLine("enter an integer:");
 
-            int myInt = Convert.ToInt32(Console.ReadLine());
+              int myInt = Convert.ToInt32(Console.ReadLine());
 
-            bool isLessThan10 = myInt < 10;
-            bool isBetween0and5 = (0 <= myInt) && (myInt <= 5);
+              bool isLessThan10 = myInt < 10;
+              bool isBetween0and5 = (0 <= myInt) && (myInt <= 5);
 
-            Console.WriteLine($"Integer is less than 10? {isLessThan10}");
-            Console.WriteLine($"Integer between 0 and 5? {isBetween0and5}");
-            Console.WriteLine($"Exactly one of the above is true?" + $"{isLessThan10 ^ isBetween0and5}");
+              Console.WriteLine($"Integer is less than 10? {isLessThan10}");
+              Console.WriteLine($"Integer between 0 and 5? {isBetween0and5}");
+              Console.WriteLine($"Exactly one of the above is true?" + $"{isLessThan10 ^ isBetween0and5}");
+            */
+
+            /* double balance, interestRate, targetBalance;
+             Console.WriteLine("what is ur current balance?");
+             balance = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("what is ur current annual interest rate in %?");
+             interestRate = 1 + Convert.ToDouble(Console.ReadLine()) / 100.0;
+             Console.WriteLine("what balance would you like to have?");
+             targetBalance = Convert.ToDouble(Console.ReadLine());
+             int totalyears = 0;
+             do
+             {
+                 balance += interestRate;
+                 ++totalyears;
+             } while (balance < targetBalance);
+             Console.WriteLine($"in {totalyears} year{(totalyears == 1 ? "" : "s")} " + $"you'll have a balance of {balance}.");
+            */
+            double number2 = 0;
+            Console.WriteLine("Please enter a number");
+            number2 = Convert.ToDouble(Console.ReadLine());
+
+            while (number2 == 3)
+            {
+                Console.WriteLine("Congratulations!!");
+                Console.ReadKey();
+            }
 
             Console.ReadKey();
         }
