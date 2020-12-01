7b9833b98dc6f872291b66317af6e0c28b77995b
Grace Dennis <G.Dennis@ou.edu>
2020-11-30 18:42:07 -0600

    done

---
+++ TextBox/TextBox/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox box = new TextBox();
+           
+            box.BackColor = ConsoleColor.Red;
+            box.ForeColor = ConsoleColor.Cyan;
+            box.Padding = 21;
+            box.Text = "Do you want a cup of coffee?";
+
+            box.DisplayText();
+            Console.ReadLine();
+
+            TextBox box2 = new TextBox();
+            box2.BackColor = ConsoleColor.DarkGray;
+            box2.ForeColor = ConsoleColor.Blue;
+            box2.Padding = 21;
+            box2.Text = "Yes!";
+            box2.DisplayText();
+            Console.ReadLine();
+
+
+            Console.WriteLine(">> Press any key to exit <<");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TextBox/TextBox/TextBox.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBox
+{
+    class TextBox
+    {
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public int Padding { get; set; }
+        public string Text { get; set; }
+
+
+        public TextBox()
+        {
+            BackColor = ConsoleColor.Magenta;
+            ForeColor = ConsoleColor.DarkMagenta;
+            Padding = 0;
+            Text = " ";
+        }
+
+        public TextBox(ConsoleColor backcolor, ConsoleColor forecolor, int padding, string text)
+        {
+            BackColor = backcolor;
+            ForeColor = forecolor;
+            Padding = padding;
+            Text = text;
+            
+        }
+
+        public void DisplayText()
+        {
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            Padding = 0;
+            Text = Text.PadLeft(Padding);
+            string Text2 = Text.PadRight(Text.Length + Padding);
+            string Text3 = (Text + Text2);
+
+            Console.WriteLine(Text3);
+            return;
+        }
+
+    }
+}
